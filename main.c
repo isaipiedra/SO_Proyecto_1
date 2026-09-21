8bb36f186408d9d934af3d30271145671cdd5649
Justin <jusamr05@gmail.com>
2026-09-21 11:24:26 -0600

    only one directory permited

--- main.c
+++ main.c
@@ -11,9 +11,74 @@ static GtkFileDialog* file_dialog = NULL;
 
 extern GtkLabel* lbl_output_folder_name;
 
+typedef struct{
+    GtkWidget* layout_holder;
+    int child;
+}ON_TOGGLE_MODE_BUTTON_CLICKED_PARAMETERS;
+
+static void on_toggle_mode_button_clicked(GtkWidget* button, gpointer user_data){
+    (void) button;
+    ON_TOGGLE_MODE_BUTTON_CLICKED_PARAMETERS * parameters = 
+        (ON_TOGGLE_MODE_BUTTON_CLICKED_PARAMETERS*) user_data;
+
+    int child = parameters->child;
+    GtkWidget* layout_holder = parameters->layout_holder;
+    GtkWidget* layout = gtk_widget_get_first_child(layout_holder);
+    int i = 1;
+    while ((layout = gtk_widget_get_next_sibling(layout))){
+        if(i == child){
+            gtk_widget_set_visible(layout, TRUE);
+        }else{
+            gtk_widget_set_visible(layout, FALSE);
+        }
+        i++;
+    }
+        printf("\n");
+
+}
 static void set_up_widgets(GtkBuilder* builder){
 
     GdkCursor* pointer_cursor = gdk_cursor_new_from_name("pointer", NULL);
+
+    window = gtk_builder_get_object(builder, "window");
+
+    // ------------- header mode files -------------
+
+    GObject* btn_compress_mode = gtk_builder_get_object(builder, "btn_compress_mode");
+    GObject* btn_decompress_mode = gtk_builder_get_object(builder, "btn_decompress_mode");
+
+    gtk_widget_set_cursor(GTK_WIDGET(btn_compress_mode), pointer_cursor);
+    gtk_widget_set_cursor(GTK_WIDGET(btn_decompress_mode), pointer_cursor);
+
+    gtk_toggle_button_set_group(GTK_TOGGLE_BUTTON(btn_compress_mode), GTK_TOGGLE_BUTTON(btn_decompress_mode));
+
+    GObject* layout_holder = gtk_builder_get_object(builder, "layout_holder");
+
+    ON_TOGGLE_MODE_BUTTON_CLICKED_PARAMETERS* on_toggle_compress_mode_parameters = 
+        g_new0(ON_TOGGLE_MODE_BUTTON_CLICKED_PARAMETERS, 1);
+
+    on_toggle_compress_mode_parameters->layout_holder = GTK_WIDGET(layout_holder);
+    on_toggle_compress_mode_parameters->child = 1;
+    g_signal_connect_data(
+        btn_compress_mode, 
+        "toggled", 
+        G_CALLBACK(on_toggle_mode_button_clicked), 
+        on_toggle_compress_mode_parameters,
+        free_callback_data, 0
+    );
+
+    ON_TOGGLE_MODE_BUTTON_CLICKED_PARAMETERS* on_toggle_decompress_mode_parameters = 
+        g_new0(ON_TOGGLE_MODE_BUTTON_CLICKED_PARAMETERS, 1);
+
+    on_toggle_decompress_mode_parameters->layout_holder = GTK_WIDGET(layout_holder);
+    on_toggle_decompress_mode_parameters->child = 2;
+    g_signal_connect_data(
+        btn_decompress_mode, 
+        "toggled", 
+        G_CALLBACK(on_toggle_mode_button_clicked), 
+        on_toggle_decompress_mode_parameters,
+        free_callback_data, 0
+    );
 
     // ------------- file explorer -------------
 
@@ -25,7 +90,7 @@ static void set_up_widgets(GtkBuilder* builder){
 
     file_collection = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, NULL);
     GtkWidget *dnd_box = GTK_WIDGET(gtk_builder_get_object(builder, "box_dnd_container"));
-    set_drop_in_box(dnd_box, file_collection, GTK_WIDGET(box_file_explorer_container));
+    set_drop_in_box(dnd_box, file_collection, GTK_WIDGET(box_file_explorer_container), GTK_WINDOW(window));
 
     // ------------- browse button -------------
     
@@ -117,14 +182,14 @@ static void activate(GtkApplication *app) {
     GtkBuilder *builder = gtk_builder_new();
     gtk_builder_add_from_file(builder, "UI/builder.ui", NULL);
 
-    window = gtk_builder_get_object(builder, "window");
+    set_up_widgets(builder);
+
     if (!window) {
         g_error("Failed to get window from builder");
         return;
     }
     gtk_window_maximize(GTK_WINDOW(window));
 
-    set_up_widgets(builder);
     
     gtk_window_set_application(GTK_WINDOW(window), app);
     gtk_widget_set_visible(GTK_WIDGET(window), TRUE);
